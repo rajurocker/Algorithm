2cc4dc459d8d8515f1e718d84fbec0fccc12043e
Rahul <rajurocker1999@gmail.com>
2019-10-23 15:00:59 +1300

    Its algorithm

---
+++ js/script.js
@@ -0,0 +1,66 @@
+console.log('Algorithm')
+
+
+var txt = "Rajurocker";
+document.getElementById("result").innerHTML = txt.length;
+
+
+
+
+
+
+
+
+
+
+// function reverseString(str) {
+// 	var reverseArray = splitString.reverse();  
+
+// }
+
+// reverseString("Rajurocker");
+
+
+
+// function reverseString(str) {
+//     var newString = "";
+    
+// }
+
+// reverseString('Rajurocker');
+
+
+
+
+/*
+
+var stringArray = [];
+
+document.getElementById("reverseString").addEventListener('click' , function(){
+	var stringInput = document.getElementById("stringInput").value;
+	console.log(stringInput);
+	var stringInput = stringInput.toLowerCase();
+	var stringArray = stringInput.split("");
+	console.log(stringArray);
+	var stringArray = stringArray.reverse();
+	console.log(stringArray);
+	var newString = stringArray.join("");
+	console.log(newString);
+
+	if (newString == stringInput){
+		alert('IS lit');
+	} else {
+		alert('It is not lit')
+	}
+});
+
+*/
+
+
+
+
+
+
+
+
+
